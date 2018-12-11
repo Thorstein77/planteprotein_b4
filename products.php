8f7bea409f0d410c24ed5000b7bde9947ed5cfb3
Thorstein77 <jubbelidiot@gmail.com>
2018-12-11 12:56:36 +0100

    Struktur

---
+++ products.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<!-- Fortæller det er html5 -->
+<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
+<html lang="da">
+
+<!-- I <head> har man opsætning - alt det som man ikke ser som selve indholdet -->
+<head>
+    <?php
+    require ("php/head.php");
+    ?>
+<title>Sigende titel</title>
+</head>
+
+<!-- i <body> har man alt indhold på siden -->
+<body>
+
+<?php
+require ("php/header.php");
+?>
+
+<main class="products">
+    <h1></h1>
+
+    <p>
+
+    </p>
+
+    <div>
+        <input>
+
+        <i class="fas fa-search"></i>
+    </div>
+
+    <aside>
+        <h2></h2>
+
+        <div>
+            <div>
+                <h3></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div>
+                <h3></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+
+            <div>
+                <h3></h3>
+
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+    </aside>
+
+    <section>
+        <div>
+            <h2></h2>
+
+            <img>
+
+            <div>
+                <h3></h3>
+
+                <div>
+                    <div>
+                        <img>
+                        <img>
+                        <img>
+                        <img>
+                    </div>
+
+                    <p>pris</p>
+                </div>
+            </div>
+
+            <button>Læg i kurv</button>
+        </div>
+    </section>
+</main>
+
+<?php
+require ("php/footer.php");
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript"></script>
+
+</body>
+</html>
