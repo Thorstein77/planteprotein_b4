5b181cd276ef91ec829910cc9be1c7e5cbb74aec
Thorstein77 <jubbelidiot@gmail.com>
2018-12-11 10:53:56 +0100

    Struktur

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <!-- Sikrer den vises korrekt på mobil, tablet mv. ved at tage ift. skærmstørrelse -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700|Quicksand:400,700" rel="stylesheet">
 
 </head>
 
