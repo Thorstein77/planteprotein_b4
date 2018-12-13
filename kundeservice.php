23fb37dad4a74543958868ab2ef8a55a1bd3ff4b
Casper <cadlh@hotmail.com>
2018-12-13 10:02:20 +0100

    Kundeservice

---
+++ kundeservice.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<!-- Fortæller det er html5 -->
+<!-- html starter og slutter hele dokumentet / lang=da: Fortæller siden er på dansk -->
+<html lang="da">
+
+<!-- I <head> har man opsætning - alt det som man ikke ser som selve indholdet -->
+<head>
+    <?php
+    require ("php/head.php");
+    ?>
+    <!-- Titel som ses oppe i browserens tab mv. -->
+    <title>Kundeservice</title>
+
+</head>
+
+<!-- i <body> har man alt indhold på siden -->
+<body>
+
+<?php
+require ("php/header.php");
+?>
+
+<main class="kundeservice">
+
+    <h1 class="kundeserviceOverskrift">Kundeservice</h1>
+
+
+
+
+</main>
+
+<?php
+require ("php/footer.php");
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript"></script>
+
+</body>
+</html>
